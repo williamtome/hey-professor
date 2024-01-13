5f80f0a73ec29a2137f9ad3f4edd33942d2248a8
William Tomé <william.weirich@helioprint.com.br>
2024-01-13 16:31:43 -0300

    It should check if ends with question mark?

--- app/Http/Controllers/QuastionController.php
+++ app/Http/Controllers/QuastionController.php
@@ -10,7 +10,17 @@ class QuastionController extends Controller
     public function store()
     {
         $attributes = request()->validate([
-            'question'=> ['required', 'min:10'],
+            'question'=> [
+                'required',
+                'min:10',
+                function ($attribute, $value, $fail) {
+                    $lastCharacter = $value[strlen($value) - 1];
+
+                    if ($lastCharacter !== '?') {
+                        $fail('Are you sure that is a question? It is missing the question mark in the end.');
+                    }
+                }
+            ],
         ]);
 
         Question::query()->create($attributes);
--- tests/Feature/CreateQuestionTest.php
+++ tests/Feature/CreateQuestionTest.php
@@ -22,7 +22,17 @@ it('should be able to create a new question bigger than 255 characters', functio
 });
 
 it('should check if ends with question mark ?', function () {
+    $user = User::factory()->create();
+    actingAs($user);
 
+    $request = post(route('question.store'), [
+        'question' => str_repeat('*', 10),
+    ]);
+
+    $request->assertSessionHasErrors([
+        'question' => 'Are you sure that is a question? It is missing the question mark in the end.'
+    ]);
+    assertDatabaseCount('questions', 0);
 });
 
 it('should have at least 10 characters', function () {
